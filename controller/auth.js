5c395cedea00a0b40df1e18d0a206cdbf62584ec
RohitSignIn <rohitsinghsignin@gmail.com>
2023-10-28 17:40:00 +0530

    Some changes in cors

--- controller/auth.js
+++ controller/auth.js
@@ -1,50 +1,53 @@
-const User = require('../model/user');
-const jwt = require('jsonwebtoken');
+const User = require("../model/user");
+const jwt = require("jsonwebtoken");
 
 const ALL_LOGIN_USER = [];
 
 module.exports.login = (req, res) => {
-	console.log(req.body)
-	const username = req.body.username;
-	const password = req.body.password;
-	if (username && password) {
-		User.findOne({
-			username: username,
-			password: password,
-		})
-			.then((user) => {
-				if (user) {
-					ALL_LOGIN_USER.push(username);
-					// return res.json({
-					// 	token: jwt.sign({ id: user.id, user: username }, 'secret_key'),
-					// });
-					let token = jwt.sign({ 
-						exp: Math.floor(Date.now() / 1000) + (60 * 60), // 1 Hour
-						data: { id: user.id, user: username }
-				 		}, process.env.ACCESS_TOKEN_SECRET);
+  console.log(req.body);
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    User.findOne({
+      username: username,
+      password: password,
+    })
+      .then((user) => {
+        if (user) {
+          ALL_LOGIN_USER.push(username);
+          // return res.json({
+          // 	token: jwt.sign({ id: user.id, user: username }, 'secret_key'),
+          // });
+          let token = jwt.sign(
+            {
+              exp: Math.floor(Date.now() / 1000) + 60 * 60, // 1 Hour
+              data: { id: user.id, user: username },
+            },
+            process.env.ACCESS_TOKEN_SECRET
+          );
 
-					res.cookie("token", token, {httpOnly: true});
-					res.cookie("user_id", user.id);
-					res.cookie("username", user.username);
-					res.cookie("email", user.email);
-					res.cookie("auth", true);
-					res.send({success: true, message: "Successfully Signed in"});
-				} else {
-					res.status(401);
-					res.send({success: false, message: "Invalid Credentials"});
-				}
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}
+          res.cookie("token", token, { httpOnly: true });
+          res.cookie("user_id", user.id);
+          res.cookie("username", user.username);
+          res.cookie("email", user.email);
+          res.cookie("auth", true);
+          res.send({ success: true, message: "Successfully Signed in" });
+        } else {
+          res.status(401);
+          res.send({ success: false, message: "Invalid Credentials" });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 };
 
 module.exports.logout = (req, res) => {
-	res.clearCookie("token");
-    res.clearCookie("user_id");
-    res.clearCookie("username");
-    res.clearCookie("email");
-    res.clearCookie("auth");
-    res.send({success: true, message: "Successfully Signed out"});
-}
+  res.clearCookie("token");
+  res.clearCookie("user_id");
+  res.clearCookie("username");
+  res.clearCookie("email");
+  res.clearCookie("auth");
+  res.send({ success: true, message: "Successfully Signed out" });
+};
